531d507255ff75947c1b13b45f02e1ac5bf71f18
jrocia <69405400+jrocia@users.noreply.github.com>
2022-11-23 16:00:01 -0300

    Update launcher.py

--- handlers/launcher.py
+++ handlers/launcher.py
@@ -1,6 +1,9 @@
-import subprocess, sys, time
+import subprocess, sys, time, re
 
 def handle(webhookObj, data):
-    p = subprocess.Popen(["pwsh.exe","-File","importScanResults.ps1"],stdout=sys.stdout)
+    pattern=r"(?<=reports\/)[^']*"
+    file=re.search(pattern,str(data))
+    file=file.group(0)
+    print(file)
+    p = subprocess.Popen(["pwsh.exe","-File","importScanResults.ps1",file],stdout=sys.stdout)
     p.communicate()
-    time.sleep(10)
